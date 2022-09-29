fc36e91274d247c108dc8a6e0cde6a8bf0f05da0
“faiz-appinventiv” <mohammad.faiz@appinventiv.com>
2022-09-29 13:18:11 +0530

    minor

--- src/components/audioVideoCall/modules/call/styles.ts
+++ src/components/audioVideoCall/modules/call/styles.ts
@@ -2,7 +2,9 @@ import {StyleSheet} from 'react-native';
 import {vh, vw} from '../../utils/Dimension';
 
 export const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+  },
   profileImageContainer: {
     height: '100%',
     width: '100%',
